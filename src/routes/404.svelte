a5786199d1b17129410135f466a15a117b4c8557
vdud <77187144+vdud@users.noreply.github.com>
2022-05-05 10:58:12 +0530

    Add files via upload

---
+++ src/routes/404.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { t } from "precompile-intl-runtime";
+</script>
+<section>
+<p>
+  Sorry buddy, must be something wrong, try refreshing or maybe come later?</p>
+</section>
+<style>
+  div {
+    overflow: scroll;
+    height: 100%;
+  }
+
+  section {
+    height: 100vh;
+    background: #ECF0F3;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 53px 5vw;
+    color: #ECF0F3;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: clamp(40px, 6.7vw, 80px);
+    padding-top: 53px;
+  }
+
+  p {
+    font-family: ElefantDisplay;
+    font-size: clamp(40px, 6.7vw, 80px);
+    color: #111111;
+    background: -webkit-linear-gradient(rgb(212, 212, 212), rgb(95, 95, 95));
+                                -webkit-background-clip: text;
+                                -webkit-text-fill-color: transparent;
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+</script>
+
+
+<slot />
+
+
+<style>
+  
+  
+</style>

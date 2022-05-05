6a2b490084146118911ea339aadb9a10b40d98df
vdud <77187144+vdud@users.noreply.github.com>
2022-05-05 11:04:15 +0530

    Create Footer.svelte

---
+++ src/lib/UI/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { slide } from 'svelte/transition';
+	import { quartInOut } from 'svelte/easing';
+	import FooterContactWindow from '$lib/Windows/FooterWindows/FooterContactWindow.svelte';
+	import FooterLanguageWindow from '$lib/Windows/FooterWindows/FooterLanguageWindow.svelte';
+	import FooterPrivacyWindow from '$lib/Windows/FooterWindows/FooterPrivacyWindow.svelte';
+	import FooterLegalWindow from '$lib/Windows/FooterWindows/FooterLegalWindow.svelte';

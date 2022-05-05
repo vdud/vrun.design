4c22c9d236124bd7cddf38bc43264b3323619d26
vdud <77187144+vdud@users.noreply.github.com>
2022-05-05 11:07:02 +0530

    Update Navigation.svelte

--- src/lib/UI/Navigation.svelte
+++ src/lib/UI/Navigation.svelte
@@ -1,10 +1,1 @@
-<script>
-	import Button from './Button.svelte';
-	import { fly } from 'svelte/transition';
-	import { t } from 'svelte-intl-precompile';
-	import navState from '$lib/UX/nav-state';
-	export let currentPage;
-	export let nextLink;
-	export let previousLink;
-	export let innerWidth;
-</script>
+

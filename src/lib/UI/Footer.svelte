546db7838cb5b533503eb7f9c97910ac1c96f828
vdud <77187144+vdud@users.noreply.github.com>
2022-05-05 11:06:50 +0530

    Update Footer.svelte

--- src/lib/UI/Footer.svelte
+++ src/lib/UI/Footer.svelte
@@ -1,14 +1,1 @@
-<script>
-	import { slide } from 'svelte/transition';
-	import { quartInOut } from 'svelte/easing';
-	import FooterContactWindow from '$lib/Windows/FooterWindows/FooterContactWindow.svelte';
-	import FooterLanguageWindow from '$lib/Windows/FooterWindows/FooterLanguageWindow.svelte';
-	import FooterPrivacyWindow from '$lib/Windows/FooterWindows/FooterPrivacyWindow.svelte';
-	import FooterLegalWindow from '$lib/Windows/FooterWindows/FooterLegalWindow.svelte';
-	export let currentPage;
-	let display = 'overview';
-	$: currentPage, (() => (display = 'overview'))();
-	function toggleWindow(name) {
-		display = display === name ? 'overview' : name;
-	}
-</script>
+
